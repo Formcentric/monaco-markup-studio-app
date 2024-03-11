eec349419f8a026e6b71dca647ce4605093ab59a
Oliver Lehmann-Korn <oliver.lehmann-korn@monday-consulting.com>
2024-03-11 12:17:03 +0100

    Update cm-utils.d.ts

--- app/cm-utils.d.ts
+++ app/cm-utils.d.ts
@@ -1,4 +1,6 @@
 import CapSession from '@coremedia/studio-client.cap-rest-client/common/CapSession';
 import RemoteBean from '@coremedia/studio-client.client-core/data/RemoteBean';
+import { ServiceDescriptorWithProps } from "@coremedia/service-agent";
 export declare function getSession(): Promise<CapSession>;
 export declare function loadRemoteBean<T extends RemoteBean>(remoteBean: T): Promise<T>;
+export declare function useService<T>(serviceDesc: ServiceDescriptorWithProps<T>): T | null;
